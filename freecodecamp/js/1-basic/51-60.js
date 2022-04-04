9e99780cbe004bb5bf1803279bfe69d500a7bb08
Alexander Volkov <AV-html@yandex.ru>
2022-04-04 17:24:50 +0300

    7-freecodecamp: 57 tasks

---
+++ freecodecamp/js/1-basic/51-60.js
@@ -0,0 +1,143 @@
+// * ===== 51 task ===== * //
+// Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+// Call the function with two numbers as arguments.
+
+function functionWithArgs(param1, param2) {
+    console.log(param1 + param2);
+}
+functionWithArgs(3, 1);
+
+
+// * ===== 52 task ===== * //
+// Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+
+function timesFive(num) {
+    return num * 5;
+}
+timesFive(5);
+
+
+// * ===== 53 task ===== * //
+// Using let or const, declare a global variable named myGlobal outside of any function. Initialize it with a value of 10.
+// Inside function fun1, assign 5 to oopsGlobal without using the let or const keywords.
+
+// Declare the myGlobal variable below this line
+let myGlobal = 10;
+
+function fun1() {
+    // Assign 5 to oopsGlobal Here
+    oopsGlobal = 5;
+}
+
+// Only change code above this line
+
+function fun2() {
+    var output = "";
+    if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+        output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+}
+
+
+
+
+// * ===== 54 task ===== * //
+
+// The editor has two console.logs to help you see what is happening. Check the console as you code to see how it changes. Declare a local variable myVar inside myLocalScope and run the tests.
+
+// Note: The console will still display ReferenceError: myVar is not defined, but this will not cause the tests to fail.
+
+function myLocalScope() {
+    let myVar;
+    // Only change code below this line
+
+    console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
+
+
+
+
+
+// * ===== 55 task ===== * //
+// Add a local variable to myOutfit function to override the value of outerWear with the string sweater.
+
+
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+    // Only change code below this line
+    let outerWear = "sweater";
+    // Only change code above this line
+    return outerWear;
+}
+
+myOutfit();
+
+
+
+// * ===== 56 task ===== * //
+// Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned value is undefined.
+
+// Setup
+let sum = 0;
+
+function addThree() {
+    sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+    sum = sum + 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
+
+
+// * ===== 57 task ===== * //
+
+// Call the processArg function with an argument of 7 and assign its return value to the variable processed.
+
+// Setup
+let processed = 0;
+
+function processArg(num) {
+    return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
+
+
+// * ===== 58 task ===== * //
+
+// Stand in Line
+// In Computer Science a queue is an abstract Data Structure where items are kept in order. New items can be added at the back of the queue and old items are taken off from the front of the queue.
+
+// Write a function nextInLine which takes an array (arr) and a number (item) as arguments.
+
+// Add the number to the end of the array, then remove the first element of the array.
+
+// The nextInLine function should then return the element that was removed.
+
+
+
+// * ===== 59 task ===== * //
+
+
+
+// * ===== 60 task ===== * //
+
+
