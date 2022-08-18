caf06186a13218d0c09303618c57ed9bf56129b8
Alexander Volkov <AV-html@yandex.ru>
2022-08-18 23:14:40 +0300

    8-kyu: contamination

---
+++ codewars/8-kyu/30-contamination.js
@@ -0,0 +1,14 @@
+function contamination(text, char) {
+    return text
+        .split('')
+        .map(() => char)
+        .join('')
+}
+
+
+
+console.log(contamination("abc", "z"));           // "zzz"
+console.log(contamination("", "z"));              // ""
+console.log(contamination("abc", ""));            // ""
+console.log(contamination("_3ebzgh4", "&"));      // "&&&&&&&&"
+console.log(contamination("//case", " "));        // "      "
